51b47f61c61d05a59ce2b9bafba20dce1d76e3c0
Subh <subhadeep0326@gmail.com>
2022-01-20 00:04:07 +0530

    [Subhadeep] add producer script to obtain stock data from api

---
+++ Stock Data/producer.py
@@ -0,0 +1,34 @@
+import json
+import requests
+from kafka import KafkaProducer
+import os
+import csv
+from dotenv import load_dotenv
+
+
+load_dotenv('.env')
+
+Key = os.getenv('API')
+
+URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=1min&slice=year1month1&apikey={}'.format(Key)
+
+topic = "PythonTest"
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                        value_serializer= lambda x: json.dumps(x).encode('utf-8'))
+
+#we are converting to csv first, and then sending them after encoding to json format.
+#this is done so that we could send the data in a proper format and in rows.
+
+with requests.Session() as s:
+    downloaded = s.get(URL)
+    rows = downloaded.content.decode('utf-8')
+    cr = csv.reader(rows.splitlines(), delimiter=',')
+    my_list = list(cr)
+    for row in my_list:
+        print(row)
+        producer.send(topic,row)
+    
+
+
+
